94e895372fa5693a489799e80e8217285f3634b9
kingthy@gmail.com <kingthy@gmail.com@e1b9f2e6-8584-11de-8681-f791fa4a598b>
2009-08-18 02:30:57 +0000

    VTemplate.WebTester例子项目增加共同模版块的示例

---
+++ src/VTemplate.WebTester/commontemplate_test.ashx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using VTemplate.Engine;
+using System.Text;
+using System.Collections.Generic;
+
+namespace VTemplate.WebTester
+{
+    /// <summary>
+    /// 共同模版块测试例子
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class commontemplate_test : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            TemplateDocument document = new TemplateDocument(context.Server.MapPath("template/commontemplate_test.html"), Encoding.UTF8);
+
+            //定义男和女用户数据列表(当前这里的数据.你可以从数据库取得)
+            Dictionary<string, List<object>> userTables = new Dictionary<string, List<object>>();
+            List<object> users1 = new List<object>()
+            {
+                new {Name = "张三", age="20"},
+                new {Name = "李四", age="24"},
+                new {Name = "王五", age="30"},
+                new {Name = "陈六", age="28"}
+            };
+            userTables.Add("男", users1);
+
+            List<object> users2 = new List<object>()
+            {
+                new {Name = "黄丽", age="18"},
+                new {Name = "张燕", age="20"},
+                new {Name = "陈梅", age="23"},
+                new {Name = "李花", age="19"}
+            };
+            userTables.Add("女", users2);
+
+
+            //获取名称为usertable的模版块
+            ElementCollection<Template> userTemplates = document.GetChildTemplatesByName("usertable");
+            foreach (Template template in userTemplates)
+            {
+                string userType = template.Attributes.GetValue("usertype");
+                if (userType != null && userTables.ContainsKey(userType))
+                {
+                    //测试当前模版块的users变量值
+                    template.Variables.SetValue("users", userTables[userType]);
+                }
+            }
+
+            document.Render(context.Response.Output);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
